f1062072182f35884e0545b31afa26c1a2c86611
ShibaLight <80370443+ShibaLight@users.noreply.github.com>
2021-03-09 22:06:32 -0600

    Add files via upload

---
+++ week03-2/main.cpp
@@ -0,0 +1,16 @@
+#include <GL/glut.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glColor3f(1,1,0);
+    glutSolidSphere(0.5,30,30);
+    glutSwapBuffers();
+}
+int main(int argc,char**argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("Week03-circle");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
